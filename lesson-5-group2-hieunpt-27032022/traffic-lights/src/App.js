90fd8e93fd780703795893d689235e3f4932ed49
owofuyuki <hieu.npt1710@gmail.com>
2022-04-19 10:58:28 +0700

    Lesson 5

--- lesson-5-group2-hieunpt-27032022/traffic-lights/src/App.js
+++ lesson-5-group2-hieunpt-27032022/traffic-lights/src/App.js
@@ -1,45 +1,24 @@
 import {useState} from "react";
+import './App.css';
 
-const Counter = () => {
+const TrafficLights = () => {
   const countState = useState(0);
   const count = countState[0];
   const setCount = countState[1];
   // const [count, setCount] = useState(0);
-  // Sử dụng const thay vì let để biết việc gán lại dữ liệu cho state là không thể
 
   console.log("countState: ", countState);
 
-  const onIncrease = () => { setCount(count + 1); };
-
-  const onIncreaseMouseOver = () => { console.log("Mouse just passed!"); };
+  const changeColor = () => { setCount(count + 1); };
 
   return (
-    <div style={{marginBottom: 8}}>
-      <span>{count}</span>
-      <button style={{marginLeft: 8}} onClick={onIncrease} onMouseOver={onIncreaseMouseOver}>Increase</button>
-    </div>
-  );
-};
-
-const Person = () => {
-  const [person, setPerson] = useState({
-    name: "Fuyuki",
-    age: 20,
-  });
-
-  const onIncreaseAge = () => { setPerson({...person, age: person.age + 1}); };
-  // const onIncreaseAge = () => { setPerson((prev) => { return { ...prev, age: prev.age + 1 }; }) };
-  /* 
-  Sử dụng spread operator để copy toàn bộ giá trị của person qua một object mới,
-  sau đó sửa lại giá trị age tương ứng. Lúc này, giá trị cũ và giá trị mới là hai object khác nhau. 
-  React sẽ nhận biết được và cập nhật lại giao diện.
-  -> Luôn truyền vào một object mới có địa chỉ ô nhớ mới để React nhận biết
-  */
-
-  return (
-    <div style={{display: "flex", marginBottom: 8, lineHeight: 0}}>
-      <p>Hello, my name is {person.name} and i'm {person.age}!</p>
-      <button style={{marginLeft: 8}} onClick={onIncreaseAge}>Increase Age</button>
+    <div>
+      <button className="btn" onClick={changeColor}>Next</button>
+      <ul className="traffic-lights">
+        <li className="light"></li>
+        <li className="light"></li>
+        <li className="light"></li>
+      </ul>
     </div>
   );
 };
@@ -47,12 +26,7 @@ const Person = () => {
 function App() {
   return (
     <div>
-      <Person />
-      <Counter />
-      <Counter />
-      <Counter />
-      {/* Ba <Counter /> là độc lập và khác nhau */}
-
+      <TrafficLights />
     </div>
   );
 }
