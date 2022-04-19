2933d8f9d20ccf5a90219b10c9bafdfc163a2922
owofuyuki <hieu.npt1710@gmail.com>
2022-04-19 15:14:38 +0700

    Lesson 5

--- lesson-5-group2-hieunpt-27032022/traffic-lights/src/App.js
+++ lesson-5-group2-hieunpt-27032022/traffic-lights/src/App.js
@@ -1,32 +1,57 @@
 import {useState} from "react";
 import './App.css';
 
-const TrafficLights = () => {
-  const countState = useState(0);
-  const count = countState[0];
-  const setCount = countState[1];
-  // const [count, setCount] = useState(0);
-
-  console.log("countState: ", countState);
-
-  const changeColor = () => { setCount(count + 1); };
-
+const Button = ({ index, onClick }) => {
   return (
-    <div>
-      <button className="btn" onClick={changeColor}>Next</button>
-      <ul className="traffic-lights">
-        <li className="light"></li>
-        <li className="light"></li>
-        <li className="light"></li>
-      </ul>
+    <button className={`btn ${index}`} onClick={onClick}>
+      { index }
+    </button>);
+};
+
+const Buttons = ({ children }) => {
+  return (
+    <div className="btn-wrapped">
+      { children }
     </div>
-  );
+  )
+};
+
+const Light = ({ color }) => {
+  return <li className={`light ${color}`}></li>;
+};
+
+const TrafficLights = ({ children }) => {
+  return (
+    <ul className="traffic-lights">
+      { children }
+    </ul>);
 };
 
 function App() {
+  const [light, setLight] = useState(0);
+
+  const backClick = () => {
+    if (light === 0) setLight(light + 2);
+    else setLight(light - 1);
+  };
+
+  const nextClick = () => {
+    if (light === 2) setLight(light - 2);
+    else setLight(light + 1);
+  };
+
   return (
-    <div>
-      <TrafficLights />
+    <div className="container">
+      <h1 className="title">A Simple Traffic Lights</h1>
+      <Buttons>
+        <Button index="Back" onClick={backClick} />
+        <Button index="Next" onClick={nextClick} />
+      </Buttons>
+      <TrafficLights>
+        <Light color={light === 0 ? "red" : ""} />
+        <Light color={light === 1 ? "yellow" : ""} />
+        <Light color={light === 2 ? "green" : ""} />
+      </TrafficLights>
     </div>
   );
 }
