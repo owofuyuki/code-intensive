3a39053220df46efd74a5ba62bcf328ed6e93d47
owofuyuki <hieu.npt1710@gmail.com>
2022-08-17 09:21:26 +0700

    Lesson 14

--- lesson-14-group2-hieunpt-22042022/class-based-comps/src/App.js
+++ lesson-14-group2-hieunpt-22042022/class-based-comps/src/App.js
@@ -9,4 +9,40 @@ function App() {
   </div>;
 }
 
+// import { Component } from "react";
+
+// const App = () => {
+//   return (
+//     <ErrorBoundary>
+//       <Counter />
+//     </ErrorBoundary>
+//   );
+// };
+
+// const Counter = () => {
+//   throw new Error("Something wrong");
+//   return <button>Counter</button>;
+// };
+
+// class ErrorBoundary extends Component {
+//   state = {
+//     hasError: false,
+//   };
+
+//   static getDerivedStateFromError(error) {
+//     return { hasError: true };
+//   }
+
+//   componentDidCatch(err, errInfo) {
+//     console.log(err, errInfo);
+//   }
+
+//   render() {
+//     if (this.state.hasError) {
+//       return <div>Something went wrong</div>;
+//     }
+//     return this.props.children;
+//   }
+// }
+
 export default App;
