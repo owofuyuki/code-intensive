8449ea2214b757f6576968e4e923b307fa05fab4
owofuyuki <hieu.npt1710@gmail.com>
2022-08-18 16:07:45 +0700

    Lesson 15

--- lesson-15-group2-hieunpt-24042022/drag-and-drop-with-react-dnd/src/App.js
+++ lesson-15-group2-hieunpt-24042022/drag-and-drop-with-react-dnd/src/App.js
@@ -1,10 +1,54 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { DndProvider, useDrag } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+
+const Square = (props) => {
+  return <div className="square">{props.hasKing ? <King /> : ""}</div>;
+};
+
+const King = () => {
+  const [dragState, drag] = useDrag(() => ({
+    type: "KING",
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  }));
+
+  return (
+    <span style={{ fontSize: 30, cursor: "pointer" }} ref={drag}>
+      ♚
+    </span>
+  );
+};
 
 function App() {
+  const [board, setBoard] = useState([
+    ["", "", ""],
+    ["", 1, ""],
+    ["", "", ""]
+  ]);
+
   return (
-    <div>
-      
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      {board.map((row, rowIdx) => {
+        return (
+          <div className="row" key={rowIdx}>
+            {row.map((col, colIdx) => {
+              return (
+                <Square
+                  key={colIdx}
+                  hasKing={col === 1}
+                  x={rowIdx}
+                  y={colIdx}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
+    </DndProvider>
   );
 }
 
